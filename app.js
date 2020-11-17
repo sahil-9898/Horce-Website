dc7e9fa34ed2f90eec44fe3b45d462fdd6661bb6
sahil-9898 <sahiltiwari.9898@gmail.com>
2020-11-17 17:18:57 +0530

    first commit

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static("public"))
+
 
 app.get('/', function(req, res){
-    res.send("hi");
+    res.render("home.ejs");
 });
 
+
+
+
+
+
+app.get("*", function(req, res){
+    res.send("Page Under Construction");
+});
 
 app.listen(3000, function(){
     console.log('Server started on localhost:3000');
